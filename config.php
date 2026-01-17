f723833c1e7eebaf5ecb7083aabe94b6f0de0c3f
finnaib <shoaibwwe01@gmail.com>
2026-01-18 01:12:59 +0200

    Fix expired finn user account

--- config.php
+++ config.php
@@ -36,7 +36,7 @@ $server_config = [
 // Username => [Password, Created_At (Optional Timestamp)]
 // "1672531200" = Jan 1, 2023. If empty, defaults to Dynamic.
 $users_db = [
-    "finn" => ["password" => "finn123", "created_at" => 1735689600], // Example: Jan 1 2025
+    "finn" => ["password" => "finn123", "created_at" => null], // Dynamic (valid for 1 year from first login)
     "tabby" => ["password" => "tabby123", "created_at" => null],       // Dynamic (Always 1 year from now)
     "test" => ["password" => "test", "created_at" => null],
     "shoaibwwe01@gmail.com" => ["password" => "Fatima786@", "created_at" => null], // Admin Account
