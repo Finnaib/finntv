601561302d014765e1319ff69be1d992586b4f55
finnaib <shoaibwwe01@gmail.com>
2026-03-05 16:05:04 +0200

    Cleanup: Remove debug/test files, update upstream server

--- restart_docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Stopping old containers..."
-sudo docker-compose down
-
-echo "Rebuilding and starting new containers..."
-sudo docker-compose up --build -d
-
-echo "Done! Check status with: sudo docker ps"
--- test_api_v3.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Quick API Debugger
- */
-$_GET['username'] = 'test';
-$_GET['password'] = 'test';
-require_once 'core/player_api.php';
-// The player_api.php will call json_out() which exits.
